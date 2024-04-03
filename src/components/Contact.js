faffc8a221d2112091d9f4bf1544d9e78a84cab6
Leeann Saengsavang <125524497+lsaengsavang@users.noreply.github.com>
2024-04-03 12:56:42 -0700

    Added links to the contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,8 +5,9 @@ function Contact() {
   return (
     <div className='contact-container'>
         <h2 className='page-title'>Contact</h2>
-        <p>LinkedIn</p>
-        <p>Email</p>
+        <a className='contact-link' href='https://www.linkedin.com/in/leeann-saengsavang/' target='_blank' rel='noopener noreferrer'>LinkedIn</a>
+        <a className='contact-link' href='https://github.com/lsaengsavang' target='_blank' rel='noopener noreferrer'>GitHub</a>
+        <a className='contact-link' href='mailto:leeann.saengsavang@gmail.com'>Email</a>
     </div>
   )
 }
