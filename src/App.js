fa7e57c70d45557597f38346ac0cf61159bfec77
Leeann Saengsavang <125524497+lsaengsavang@users.noreply.github.com>
2024-04-02 19:28:28 -0700

    Moved About contents to the Navbar.js

--- src/App.js
+++ src/App.js
@@ -4,28 +4,17 @@ import Contact from './components/Contact';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
-import me from './assets/me.jpg'
 
 function App() {
   return (
     <div className="App">
-      <div className='left-side'>
-        <div className='about-container'>
-          <img src={me} className="about-image" alt="Leeann"/>
-          <h1>Leeann Saengsavang</h1>
-          <p>Full-Stack Developer</p>
-        </div>
-        <Navbar />
-      </div>
-
+      <Navbar />
       <div id="projects" className="section">
         <Projects />
       </div>
-
       <div id="skills" className="section">
         <Skills />
       </div>
-
       <div id="contact" className="section">
         <Contact />
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../css/Navbar.css';
+import me from '../assets/me.jpg'; // Import the image here
 
 function Navbar() {
   const scrollToSection = (id) => {
@@ -10,12 +11,20 @@ function Navbar() {
   };
 
   return (
-    <div className='nav-buttons'>
-      <button onClick={() => scrollToSection('projects')}>Projects</button>
-      <button onClick={() => scrollToSection('skills')}>Skills</button>
-      <button onClick={() => scrollToSection('contact')}>Contact</button>
+    <div className='left-side'>
+      <div className='about-container'>
+        <img src={me} className="about-image" alt="Leeann"/>
+        <h1>Leeann Saengsavang</h1>
+        <p>Full-Stack Developer</p>
+      </div>
+      <div className='nav-buttons'>
+        <button onClick={() => scrollToSection('projects')}>Projects</button>
+        <button onClick={() => scrollToSection('skills')}>Skills</button>
+        <button onClick={() => scrollToSection('contact')}>Contact</button>
+      </div>
     </div>
   );
 }
 
 export default Navbar;
+
