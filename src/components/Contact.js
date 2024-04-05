009c2d1d7b83dbe8ec3c6b599ab81eb52787fd11
Leeann Saengsavang <125524497+lsaengsavang@users.noreply.github.com>
2024-04-04 20:35:22 -0700

    Added buttons to Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,21 +6,21 @@ import EmailIcon from '@mui/icons-material/Email';
 
 function Contact() {
   return (
-
     <div className='contact'>
       <div className='contact-container'>
         <h2 className='page-title'>Contact</h2>
-        <a className='contact-link' href='https://www.linkedin.com/in/leeann-saengsavang/' target='_blank' rel='noopener noreferrer'>
-          <LinkedInIcon style={{ fontSize: '30px' }} /> LinkedIn
-        </a>
-        <a className='contact-link' href='https://github.com/lsaengsavang' target='_blank' rel='noopener noreferrer'>
-          <GitHubIcon style={{ fontSize: '30px', marginRight: '5px'}} />GitHub</a>
-        <a className='contact-link' href='mailto:leeann.saengsavang@gmail.com'>
-        <EmailIcon style={{ fontSize: '30px'}} />Email</a>
+        <button className='contact-button' onClick={() => window.open('https://www.linkedin.com/in/leeann-saengsavang/', '_blank')} rel='noopener noreferrer'>
+          <LinkedInIcon style={{ fontSize: '30px', marginRight: '10px' }} /> LinkedIn
+        </button>
+        <button className='contact-button' onClick={() => window.open('https://github.com/lsaengsavang', '_blank')} rel='noopener noreferrer'>
+          <GitHubIcon style={{ fontSize: '30px', marginRight: '10px' }} /> GitHub
+        </button>
+        <button className='contact-button' onClick={() => window.location.href = 'mailto:leeann.saengsavang@gmail.com'}>
+          <EmailIcon style={{ fontSize: '30px', marginRight: '10px' }} /> Email
+        </button>
+      </div>
     </div>
-    </div>
-
-  )
+  );
 }
 
 export default Contact
